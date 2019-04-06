cb238fbf1553563f25432e642c2ccec90e236037
tharani as <tharani as@LAPTOP-CFMUGL08>
2019-04-06 18:13:33 +0530

    hai

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -1,6 +1,5 @@
 package main;
 import java.util.*; 
-
 public class Calculator {
     public static void main(String[] args)
     {
@@ -28,37 +27,26 @@ public class Calculator {
             break;
             default:
                 System.out.println("Illigal Operation");
-
-
         }
-
-
-
     }
     public static int add(int x, int y)
     {
-    	int result=0;
-//        write your code here 
-    	
-        return result;
+    	int result= x + y;
+ 	   return result;
     }
     public static int sub(int x, int y)
     {
-    	int result=0;
-//        write your code here
+    	int result= x - y;
         return result;
     }
     public static int mult(int x, int y)
     {
-    	int result=0;
-//      write your code here        
-    	return result;
+    	int result= x * y;
+       	return result;
     }
     public static int div(int x, int y)
     {
-    	int result=0;
-//      write your code here
+    	int result= x / y;
     	return result;
     }
-
 }
